761b235462d1211c039acaf59e4a682f6d3ed165
byunjaewoo <79103379+byunjaewoo@users.noreply.github.com>
2021-04-06 08:23:33 +0900

    Update byunjaewoo.cpp

--- byunjaewoo.cpp
+++ byunjaewoo.cpp
@@ -5,6 +5,22 @@ namespace byunjaewoo {
         if(a==1) return 1;
         return a * Factorial(a - 1);
     }
+    long long Pow(int a, int x) {
+        if(x==0) return 1;
+        if(x==1) return a;
+        long long res = Pow(a, x/2);
+        res *= res;
+        if(x % 2) res *= a;
+        return res;
+    }
+    long long Pow(int a, int x, int Mod) {
+        if(x==0) return 1;
+        if(x==1) return a;
+        long long res = Pow(a, x / 2) % Mod;
+        res = (res * res) % Mod;
+        if(x % 2) res = (res * a) % Mod;
+        return res;
+    }
 }
 using namespace byunjaewoo;
 
