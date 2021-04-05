f161b62fe5eb4a680400defaf6bbe3c19ce3e38b
byunjaewoo <79103379+byunjaewoo@users.noreply.github.com>
2021-04-06 01:08:20 +0900

    Update byunjaewoo.cpp

--- byunjaewoo.cpp
+++ byunjaewoo.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 namespace byunjaewoo {
-    
+    long long Factorial (int a) {
+        if(a==1) return 1;
+        return a * Factorial(a - 1);
+    }
 }
 using namespace byunjaewoo;
 
